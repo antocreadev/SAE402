9af7f48c1513880df1d4dc5b8a80658414cc6276
antò - Creative Developer <87537068+antoCreaDev@users.noreply.github.com>
2023-04-02 20:14:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-if (typeof window.DeviceMotionEvent != 'undefined') {
-    // Shake sensitivity (a lower number is more)
-    var sensitivity = 20;
+let SHAKE_THRESHOLD = 10;
+let lastX = null;
+let lastY = null;
+let lastZ = null;
+let lastUpdate = null;
 
-    // Position variables
-    var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+window.addEventListener('deviceorientation', function(event) {
+  if (lastUpdate) {
+    let timeDifference = event.timeStamp - lastUpdate;
+    if (timeDifference < 100) {
+      return;
+    }
 
-    // Listen to motion events and update the position
-    window.addEventListener('devicemotion', function (e) {
-        x1 = e.accelerationIncludingGravity.x;
-        y1 = e.accelerationIncludingGravity.y;
-        z1 = e.accelerationIncludingGravity.z;
-    }, false);
+    let deltaX = event.gamma - lastX;
+    let deltaY = event.beta - lastY;
+    let deltaZ = event.alpha - lastZ;
 
-    // Periodically check the position and fire
-    // if the change is greater than the sensitivity
-    setInterval(function () {
-        var change = Math.abs(x1-x2+y1-y2+z1-z2);
+    let speed = Math.abs(deltaX + deltaY + deltaZ - lastUpdate) / timeDifference * 10000;
 
-        if (change > sensitivity) {
-            alert('Shake!');
-        }
+    if (speed > SHAKE_THRESHOLD) {
+      document.body.style.backgroundColor = "red";
+      console.log('Shake detected!');
+    }
+  }
 
-        // Update new position
-        x2 = x1;
-        y2 = y1;
-        z2 = z1;
-    }, 150);
-}
+  lastUpdate = event.timeStamp;
+  lastX = event.gamma;
+  lastY = event.beta;
+  lastZ = event.alpha;
+});
