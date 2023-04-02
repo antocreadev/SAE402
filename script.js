f11a75ac423ccb2724786991c1f2089d077a859c
antò - Creative Developer <87537068+antoCreaDev@users.noreply.github.com>
2023-04-02 12:11:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,35 @@
 var tab_ball_r = []; // tableau qui crer les balles dégradés + position
 var tab_anime_r = []; // tableau qui va contenir les ball a animer
 var ballCount_r = 50; // nombre de balles
+let animatingRed = true;
 
 // GREEN
 var tab_ball_g = []; // tableau qui crer les balles dégradés + position
 var tab_anime_g = []; // tableau qui va contenir les ball a animer
 var ballCount_g = 50; // nombre de balles
+let animatingGreen = false;
 
 // BLUE
 var tab_ball_b = []; // tableau qui crer les balles dégradés + position
 var tab_anime_b = []; // tableau qui va contenir les ball a animer
 var ballCount_b = 50; // nombre de balles
+let animatingBlue = false;
 
 const width_ball = 80; // largeur de la balle
 // !! A FAIRE augmenter la taille de width_ball quand on est sur mobile et l'espacement des cercles
 var delay_anim = 1; // the number of frames to wait before adding a new ball (60 frames = 1 second)
+
+// function mouseClicked() {
+//   // Vérifier si le clic a eu lieu sur la balle blanche
+//   const distance = dist(mouseX, mouseY, ballWithe.x, ballWithe.y);
+//   if (distance < ballWithe.r) {
+//     console.log("Clic sur la balle blanche détecté");
+//   }
+// }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(100);
+  background(color(0, 0, 0, 0));
   // RED
   for (let i = 0; i <= ballCount_r; i++) {
     tab_ball_r[i] = new Ball(
@@ -59,47 +71,71 @@ function setup() {
   }
 }
 function draw() {
-  background(0);
+  background(color(0, 0, 0, 0));
+  const ballWithe = new Ball(
+    windowWidth / 2 + width_ball / 2,
+    windowHeight / 1.5,
+    width_ball,
+    255,
+    255,
+    255,
+    1
+  );
   if (frameCount % delay_anim == 0) {
-    // RED
-    print(frameCount, " dalay: ", delay_anim, " deltaTime: ", deltaTime);
-    tab_ball_r[ballCount_r].show();
-    tab_anime_r.push(tab_ball_r[ballCount_r]);
-    tab_anime_r.forEach((ball) => {
-      ball.show();
-    });
-    ballCount_r--;
+    if (animatingRed) {
+      // RED
+      tab_anime_r.push(tab_ball_r[ballCount_r]);
+      tab_anime_r.forEach((ball) => {
+        ball.show();
+      });
+      ballCount_r--;
 
-    // GREEN
-    tab_ball_g[ballCount_g].show();
-    tab_anime_g.push(tab_ball_g[ballCount_g]);
-    tab_anime_g.forEach((ball) => {
-      ball.show();
-    });
-    ballCount_g--;
+      if (ballCount_r == 0) {
+        animatingRed = false;
+        animatingBlue = true;
+      }
+    } else if (animatingBlue) {
+      // BLUE
+      tab_anime_b.push(tab_ball_b[ballCount_b]);
+      tab_anime_b.forEach((ball) => {
+        ball.show();
+      });
+      ballCount_b--;
 
-    // BLUE
-    tab_ball_b[ballCount_b].show();
-    tab_anime_b.push(tab_ball_b[ballCount_b]);
-    tab_anime_b.forEach((ball) => {
-      ball.show();
-    });
-    ballCount_b--;
+      if (ballCount_b == 0) {
+        animatingBlue = false;
+        animatingGreen = true;
+      }
+    } else if (animatingGreen) {
+      // GREEN
+      tab_anime_g.push(tab_ball_g[ballCount_g]);
+      tab_anime_g.forEach((ball) => {
+        ball.show();
+      });
+      ballCount_g--;
+
+      if (ballCount_g == 0) {
+        animatingGreen = false;
+        noLoop();
+      }
+    }
   }
-  if (ballCount_r == 0 && ballCount_g == 0 && ballCount_b == 0) {
-    const ball = new Ball(
-      windowWidth /1.9,
-      windowHeight / 1.5,
-      width_ball,
-      255,
-      255,
-      255,
-      1
-    );
+  // dessiner les balles qui sont dans les tableaux tab_anime_r et tab_anime_b même après la fin de l'animation
+  tab_anime_r.forEach((ball) => {
     ball.show();
+  });
+
+  tab_anime_b.forEach((ball) => {
+    ball.show();
+  });
+  ballWithe.show();
+
+  // stopper l'animation lorsque toutes les balles ont été dessinées
+  if (ballCount_r == 0 && ballCount_g == 0 && ballCount_b == 0) {
     noLoop(); // stop the animation
   }
 }
+
 class Ball {
   constructor(pos_x, pos_y, d, r, g, b, a) {
     this.red = r;
