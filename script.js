3593675a2cbc24496df909eb2adbf478f45ea8e9
antò - Creative Developer <87537068+antoCreaDev@users.noreply.github.com>
2023-04-02 18:53:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,18 @@
 if (window.DeviceMotionEvent) {
   window.addEventListener("devicemotion", deviceMotionHandler, false);
   document.body.style.backgroundColor = "yellow";
+} else if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", deviceOrientationHandler, false);
+  document.body.style.backgroundColor = "yellow";
 } else {
-  console.log("L'accéléromètre n'est pas disponible sur cet appareil.");
+  console.log("L'accéléromètre et la boussole ne sont pas disponibles sur cet appareil.");
   document.body.style.backgroundColor = "red";
 }
 
 // Fonction pour gérer les événements de mouvement de l'appareil
 function deviceMotionHandler(eventData) {
   var acceleration = eventData.accelerationIncludingGravity;
-  var threshold = 10;
+  var threshold = 2;
 
   // Calculer la force de la secousse
   var shakeX = Math.abs(acceleration.x - gravity.x);
@@ -19,6 +22,19 @@ function deviceMotionHandler(eventData) {
 
   // Si la secousse est suffisamment forte, afficher une alerte
   if (shakeX > threshold || shakeY > threshold || shakeZ > threshold) {
+    document.body.style.backgroundColor = "blue";
+  }
+}
+
+// Fonction pour gérer les événements d'orientation de l'appareil
+function deviceOrientationHandler(eventData) {
+  var threshold = 15;
+
+  // Calculer l'angle de rotation autour de l'axe Z (vertical)
+  var rotation = eventData.alpha;
+
+  // Si l'angle de rotation est suffisamment grand, afficher une alerte
+  if (rotation > threshold) {
     document.body.style.backgroundColor = "blue";
   }
 }
