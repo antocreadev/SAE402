c2e630a49a36a73c8112c893576268617d49c281
antò - Creative Developer <87537068+antoCreaDev@users.noreply.github.com>
2023-04-02 17:59:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 // Définir un seuil de secousse
-var seuilSecousse = 15;
+var seuilSecousse = 20;
 
 // Variables pour stocker la dernière position
 var lastX, lastY, lastZ;
 
 // Ajouter un écouteur d'événements pour l'événement 'devicemotion'
-window.addEventListener(
-  "devicemotion",
-  function (e) {
-    // Obtenir l'accélération sur l'axe X, Y et Z
-    var accelerationX = e.accelerationIncludingGravity.x;
-    var accelerationY = e.accelerationIncludingGravity.y;
-    var accelerationZ = e.accelerationIncludingGravity.z;
+window.addEventListener("devicemotion", function (e) {
+  // Obtenir l'accélération sur l'axe X, Y et Z
+  var accelerationX = e.accelerationIncludingGravity.x;
+  var accelerationY = e.accelerationIncludingGravity.y;
+  var accelerationZ = e.accelerationIncludingGravity.z;
 
-    // Vérifier si les valeurs de l'accélération ont changé de manière significative par rapport aux dernières valeurs
-    if (
-      Math.abs(accelerationX - lastX) > seuilSecousse ||
-      Math.abs(accelerationY - lastY) > seuilSecousse ||
-      Math.abs(accelerationZ - lastZ) > seuilSecousse
-    ) {
-      // Une secousse a été détectée
-      alert("Secousse détectée!");
-    }
+  // Vérifier si les valeurs de l'accélération ont changé de manière significative par rapport aux dernières valeurs
+  if (
+    Math.abs(accelerationX - lastX) > seuilSecousse ||
+    Math.abs(accelerationY - lastY) > seuilSecousse ||
+    Math.abs(accelerationZ - lastZ) > seuilSecousse
+  ) {
+    // Une secousse a été détectée
+    console.log("Secousse détectée!");
+  }
 
-    // Mettre à jour les dernières valeurs
-    lastX = accelerationX;
-    lastY = accelerationY;
-    lastZ = accelerationZ;
-  },
-  true
-);
+  // Mettre à jour les dernières valeurs
+  lastX = accelerationX;
+  lastY = accelerationY;
+  lastZ = accelerationZ;
+});
