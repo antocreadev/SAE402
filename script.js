8320196cfdd35ade48b87e89cfa050d9d61f4785
antò - Creative Developer <87537068+antoCreaDev@users.noreply.github.com>
2023-04-02 19:59:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,29 @@
-/*
+if (typeof window.DeviceMotionEvent != 'undefined') {
+    // Shake sensitivity (a lower number is more)
+    var sensitivity = 20;
 
+    // Position variables
+    var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
 
-Ce code utilise window.DeviceMotionEvent pour détecter les appareils Android et window.DeviceOrientationEvent pour détecter les appareils iOS. La fonction deviceMotionHandler est utilisée pour détecter une secousse sur les appareils Android, tandis que la fonction deviceOrientationHandler est utilisée pour détecter une secousse sur les appareils iOS.
+    // Listen to motion events and update the position
+    window.addEventListener('devicemotion', function (e) {
+        x1 = e.accelerationIncludingGravity.x;
+        y1 = e.accelerationIncludingGravity.y;
+        z1 = e.accelerationIncludingGravity.z;
+    }, false);
 
+    // Periodically check the position and fire
+    // if the change is greater than the sensitivity
+    setInterval(function () {
+        var change = Math.abs(x1-x2+y1-y2+z1-z2);
 
-*/
+        if (change > sensitivity) {
+            alert('Shake!');
+        }
 
-// Vérifier si l'appareil prend en charge le gyroscope
-if (window.DeviceOrientationEvent && "ontouchstart" in window) {
-  // Ajouter un écouteur d'événement pour le gyroscope
-  window.addEventListener("devicemotion", function (event) {
-    // Récupérer les données du gyroscope
-    var acceleration = event.accelerationIncludingGravity;
-    var rotation = event.rotationRate;
-
-    // Calculer la vitesse angulaire totale de l'appareil
-    var totalRotation =
-      Math.abs(rotation.alpha) +
-      Math.abs(rotation.beta) +
-      Math.abs(rotation.gamma);
-
-    // Calculer la force de la secousse
-    var shakeThreshold = 100;
-    var shakeX = Math.abs(acceleration.x) > shakeThreshold ? 1 : 0;
-    var shakeY = Math.abs(acceleration.y) > shakeThreshold ? 1 : 0;
-    var shakeZ = Math.abs(acceleration.z) > shakeThreshold ? 1 : 0;
-
-    // Si la vitesse angulaire totale ou la force de la secousse est suffisamment grande, afficher une alerte
-    if (totalRotation > shakeThreshold || shakeX + shakeY + shakeZ > 1) {
-      document.body.style.backgroundColor = "white";
-    }
-  });
-} else {
-  console.log("Le gyroscope n'est pas disponible sur cet appareil.");
+        // Update new position
+        x2 = x1;
+        y2 = y1;
+        z2 = z1;
+    }, 150);
 }
-
-// Détecter une secousse ou un mouvement de secousse
-if (window.DeviceMotionEvent) {
-  window.addEventListener("devicemotion", deviceMotionHandler, false);
-  document.body.style.backgroundColor = "green";
-} else {
-  console.log(
-    "L'accéléromètre et la boussole ne sont pas disponibles sur cet appareil."
-  );
-  document.body.style.backgroundColor = "red";
-}
-if (window.DeviceOrientationEvent) {
-  window.addEventListener("deviceorientation", deviceOrientationHandler, false);
-  document.body.style.backgroundColor = "white";
-} else {
-  console.log(
-    "L'accéléromètre et la boussole ne sont pas disponibles sur cet appareil."
-  );
-  document.body.style.backgroundColor = "red";
-}
-
-// Fonction pour gérer les événements de mouvement de l'appareil
-function deviceMotionHandler(eventData) {
-  var acceleration = eventData.accelerationIncludingGravity;
-  var threshold = 2;
-
-  // Calculer la force de la secousse
-  var shakeX = Math.abs(acceleration.x - gravity.x);
-  var shakeY = Math.abs(acceleration.y - gravity.y);
-  var shakeZ = Math.abs(acceleration.z - gravity.z);
-
-  // Si la secousse est suffisamment forte, afficher une alerte
-  if (shakeX > threshold || shakeY > threshold || shakeZ > threshold) {
-    document.body.style.backgroundColor = "blue";
-  }
-}
-
-// Fonction pour gérer les événements d'orientation de l'appareil
-function deviceOrientationHandler(eventData) {
-  var threshold = 2;
-
-  // Calculer l'angle de rotation autour de l'axe Z (vertical)
-  var rotation = eventData.alpha;
-
-  // Si l'angle de rotation est suffisamment grand, afficher une alerte
-  if (rotation > threshold) {
-    document.body.style.backgroundColor = "blue";
-  }
-}
-
-// Obtenir les valeurs de gravité
-var gravity = {
-  x: 0,
-  y: 0,
-  z: 0,
-};
-window.addEventListener(
-  "devicemotion",
-  function (event) {
-    // Récupérer les données de l'accéléromètre
-    var acceleration = event.accelerationIncludingGravity;
-
-    // Calculer la moyenne pondérée pour la gravité
-    gravity.x = gravity.x * 0.9 + acceleration.x * 0.1;
-    gravity.y = gravity.y * 0.9 + acceleration.y * 0.1;
-    gravity.z = gravity.z * 0.9 + acceleration.z * 0.1;
-  },
-  true
-);
