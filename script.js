48d086a69f8b9b70e14fba01d84b38e5ab70d127
antò - Creative Developer <87537068+antoCreaDev@users.noreply.github.com>
2023-04-03 02:48:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
-let SHAKE_THRESHOLD = 10;
-let lastX = null;
-let lastY = null;
-let lastZ = null;
-let lastUpdate = null;
+const circleWhite = document.querySelector("#circleWhite");
 
-window.addEventListener('deviceorientation', function(event) {
-  if (lastUpdate) {
-    let timeDifference = event.timeStamp - lastUpdate;
-    if (timeDifference < 100) {
-      return;
-    }
+const imgRed = document.querySelector("#imgRed");
+const imgGreen = document.querySelector("#imgGreen");
+const imgBlue = document.querySelector("#imgBlue");
+console.log(circleWhite);
 
-    let deltaX = event.gamma - lastX;
-    let deltaY = event.beta - lastY;
-    let deltaZ = event.alpha - lastZ;
-
-    let speed = Math.abs(deltaX + deltaY + deltaZ - lastUpdate) / timeDifference * 10000;
-
-    if (speed > SHAKE_THRESHOLD) {
-      document.body.style.backgroundColor = "red";
-      console.log('Shake detected!');
-    }
-  }
-
-  lastUpdate = event.timeStamp;
-  lastX = event.gamma;
-  lastY = event.beta;
-  lastZ = event.alpha;
+circleWhite.addEventListener("click", () => {
+  // add class
+  circleWhite.classList.add("animCircleWhite");
+  setTimeout(() => {
+    imgRed.classList.add("animImgRed");
+    imgGreen.classList.add("animImgGreen");
+    imgBlue.classList.add("animImgBlue");
+  }, 2000);
 });
